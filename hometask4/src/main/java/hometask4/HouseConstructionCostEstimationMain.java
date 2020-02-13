68b60acb462093799741cfedd17cd3c8e94052dd
PokalaAnkitha <50981933+PokalaAnkitha@users.noreply.github.com>
2020-02-13 16:10:08 +0530

    Add files via upload

---
+++ hometask4/src/main/java/hometask4/HouseConstructionCostEstimationMain.java
@@ -0,0 +1,10 @@
+package hometask4;
+
+class HouseConstructionCostEstimationMain
+{
+	public static void main(String args[])
+	{
+		HouseConstructionCostEstimation estimate = new HouseConstructionCostEstimation();
+		estimate.costPerSquareFeet();
+	}
+}
